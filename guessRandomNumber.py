9b81253addf2deb505476ff4edfbc0892fa89818
DBerrones <53327546+danielberrones@users.noreply.github.com>
2020-05-19 19:33:35 -0500

    Added an array for guesses

--- guessRandomNumber.py
+++ guessRandomNumber.py
@@ -5,20 +5,25 @@
 import random, time
 num = random.randint(0, 1000)
 counter = 0
+guesses=[]
 
 while True:
     user = int(input("\nGuess a random number between 0 and 1000: "))
     if user > num:
         print('Too high\n')
+        guesses.append(user)
         counter += 1
     elif user < num:
         print('Too low\n')
+        guesses.append(user)
         counter += 1
     elif user == num:
         print('You win!')
         time.sleep(3)
         print("It took you " + str(counter) + " guesses to determine the random number.")
+        print("Here are your guesses: " + str(guesses))
         with open("yourScore.text","w") as f:
-            f.write("It took you " + str(counter) + " guesses to determine the random number.") 
+            f.write("It took you " + str(counter) + " guesses to determine the random number.")
+            f.write("Here are your guesses: " + str(guesses))
         break
 
